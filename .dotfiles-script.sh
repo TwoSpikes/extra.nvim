d140ee902765f4b5c3038ea97c591169306a934e
Two Spikes <twospikesfpg@gmail.com>
2024-06-20 21:05:58 +0300

    [.dotfiles-script.sh] Fix compatibility with bash

--- .dotfiles-script.sh
+++ .dotfiles-script.sh
@@ -2,5 +2,5 @@
 dotfiles init
 alias nvim='nvim -c "let g:DO_NOT_OPEN_ANYTHING=0" -c "let g:PAGER_MODE=0" $@'
 alias q="exit"
-md(){test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1";}
-nd(){test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1" && nvim ./;}
+md(){ test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1"; }
+nd(){ test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1" && nvim ./; }
