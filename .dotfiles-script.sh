d79e43c72e0040879e3d0204c489e27bb962518a
Two Spikes <twospikesfpg@gmail.com>
2024-06-28 16:36:26 +0300

    fix(shell): Fix alias

--- .dotfiles-script.sh
+++ .dotfiles-script.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 dotfiles init
-alias nvim='nvim -c "let g:DO_NOT_OPEN_ANYTHING=0" -c "let g:PAGER_MODE=0" $@'
+alias nvim='nvim -c "let g:DO_NOT_OPEN_ANYTHING=0" -c "let g:PAGER_MODE=0"'
 alias q="exit"
 md(){ test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1"; }
 nd(){ test $# -eq 1 && mkdir -p -- "$1" && cd -- "$1" && nvim ./; }
