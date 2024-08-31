9c3e110946f17fc63c8d751738b00574c61185b8
Two Spikes <twospikesfpg@gmail.com>
2024-09-01 02:23:36 +0300

    feat: Add some keymappings to `nvim-cmp` plug-in setup

--- .config/nvim/lua/packages/nvim-cmp/setup.lua
+++ .config/nvim/lua/packages/nvim-cmp/setup.lua
@@ -19,8 +19,11 @@ mapping = cmp.mapping.preset.insert({
   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
   ['<C-f>'] = cmp.mapping.scroll_docs(4),
   ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  ["<C-CR>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scroll up
+  ["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll down
+  ["<C-e>"] = cmp.mapping.close(),
 }),
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
