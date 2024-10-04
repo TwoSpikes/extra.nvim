93048d2bd0667f3c2d2157245bb1be2782a3a452
Two Spikes <twospikesfpg@gmail.com>
2024-10-04 16:05:16 +0300

    fix: Fix bug

--- .config/nvim/lua/lib/vim/plugins.lua
+++ .config/nvim/lua/lib/vim/plugins.lua
@@ -2,5 +2,5 @@ require('lib.lists.compare')
 
 function plugin_installed(name)
 	dir = vim.fn.expand(vim.g.LOCALSHAREPATH.."/site/pack/packer/start/"..name)
-	return vim.fn.isdirectory(dir) == 1 and equals(dir, {".git"})
+	return vim.fn.isdirectory(dir) == 1 and not equals(vim.fn.readdir(dir), {".git"})
 end
